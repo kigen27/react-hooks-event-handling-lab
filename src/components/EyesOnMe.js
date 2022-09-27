5f147ce266214b56a405f74ca429cb22f5a4f7c5
Marley <andrew.kigen@student.moringaschool.com>
2022-09-27 08:29:37 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+
+function EyesOnMe() {
+	const handleBlure = (event) => {
+		console.log("Hey! Eyes on me!");
+	};
+	const handleFocus = () => {
+		console.log("Good!");
+	};
+	return (
+		<>
+			<button onBlur={handleBlure} onFocus={handleFocus}>
+				Eyes on me
+			</button>
+		</>
+	);
+}
+export default EyesOnMe;
